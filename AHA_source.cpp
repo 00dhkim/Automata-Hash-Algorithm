db20016e328f800b70d0d7055cc569b75588d53f
kimdo331 <kimdo331@naver.com>
2019-09-21 21:29:53 +0900

    함수화 (Functionalization)
    DO_transform_to_matrix()  ->격자로 변환
    DO_playCA()  ->세포 자동자 실행
    DO_transform_to_string()  ->문자로 변환
    앞으로 DO_~~() 꼴의 이름은 전부 main()의 코드를 통째로 치환하는 용도로 쓰일 예정
    i, j 등 인덱스변수나 num등 변수는 지역변수로 넣음
    input[]은 함수에서 다쓰이고, binary[]는 추후에 쓰일 예정이라 전역변수로 둠

--- AHA_source.cpp
+++ AHA_source.cpp
@@ -10,18 +10,54 @@
 #define IS_SALT 1
 
 char hashtable[64] = {
-						'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 
-						'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 
-						'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 
-						'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 
-						'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 
-						'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 
-						'W', 'X', 'Y', 'Z', '0', '1', '2', '3', 
-						'4', '5', '6', '7', '8', '9', '+', '-' 
+						'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+						'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+						'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
+						'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F',
+						'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
+						'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
+						'W', 'X', 'Y', 'Z', '0', '1', '2', '3',
+						'4', '5', '6', '7', '8', '9', '+', '-'
 };
 //BASE64 table
-char input[96+32], salt[32], output[128];
-int matrix[HEIGHT][WIDTH] = { 0, };
+char input[96 + 32], salt[32], binary[7];
+int matrix[HEIGHT][WIDTH] = { 0, }, matrix2[HEIGHT][WIDTH] = { 0, };
+
+void swap(int &a, int &b);
+int random(int a, int b);
+int CharToDec(char c);
+void PrintMatrix(const char str[]);
+void random_input_salt(int input_size, int salt_size);
+
+void DO_transform_to_matrix(void);
+void DO_playCA(void);
+void DO_transform_to_string(void);
+
+int main()
+{
+	srand((int)time(NULL));
+
+	printf("input: ");
+	scanf("%s", input);
+	if (IS_SALT) {
+		printf("salt: ");
+		scanf("%s", salt);
+		strcat(input, salt);
+	}
+	system("pause"); system("cls");
+
+//	random_input_salt(20, 5);
+
+	DO_transform_to_matrix();	// 격자로 변환
+
+	DO_playCA();	// 세포 자동자 실행
+
+	DO_transform_to_string();	// 문자로 변환
+
+	system("pause");
+
+	return 0;
+}
 
 void swap(int &a, int &b)
 {
@@ -52,7 +88,7 @@ void PrintMatrix(const char str[])
 		{
 			if (matrix[i][j]) printf("■");
 			else printf("□");
-//			if (!((j+1) % 6)) printf(" ");
+			//			if (!((j+1) % 6)) printf(" ");
 		}
 		printf("\n");
 	}
@@ -60,35 +96,17 @@ void PrintMatrix(const char str[])
 	system("cls");
 }
 
-int main()
-{
-	int i, j, k, num, t;
-	char binary[7];
-
-	srand((int)time(NULL));
-
-	printf("input: ");
-	scanf("%s", input);
-	if (IS_SALT) {
-		printf("salt: ");
-		scanf("%s", salt);
-		strcat(input, salt);
-	}
-	system("pause"); system("cls");
-
-/*	//random input & salt
-	for (i = 0; i < 96; i++)
+void random_input_salt(int input_size, int salt_size) {
+	for (int i = 0; i < input_size; i++)
 		input[i] = hashtable[random(0, 63)];
-	for (i = 0; i < 32; i++)
+	for (int i = 0; i < salt_size; i++)
 		salt[i] = hashtable[random(0, 63)];
-*/	
+}
 
-	//======================================================
-	//====================격자로 변환========================
-	
-	t = 0;
-	i = j = 0;
-	while (!(i == HEIGHT-1 && j+6 > WIDTH-1))	//matrix다 채우면 탈출하기
+void DO_transform_to_matrix(void) {	//격자로 변환
+	int t = 0, i = 0, j = 0, k, num;
+
+	while (!(i == HEIGHT - 1 && j + 6 > WIDTH - 1))	//matrix다 채우면 탈출하기
 	{
 		num = CharToDec(input[t++]);
 		strcpy(binary, "000000");
@@ -111,22 +129,20 @@ int main()
 		}
 		else {	// 바로 넣을 수 있으면
 			for (k = 0; k < 6; k++) {
-				matrix[i][j+k] = (binary[k] == '1' ? 1 : 0);
+				matrix[i][j + k] = (binary[k] == '1' ? 1 : 0);
 			}
 			j += 6;
 		}
 	}
-	
+
 	for (k = 0; k < WIDTH - j; k++) { //남은 0~5칸 채우기
 		matrix[i][j + k] = (binary[k] == '1' ? 1 : 0);
 	}
-//	PrintMatrix(":INIT_MATRIX");
+	//	PrintMatrix(":INIT_MATRIX");
+}
 
-	//====================격자로 변환========================
-	//======================================================
-	//=================세포 자동자 실행======================
-
-	int matrix2[HEIGHT][WIDTH] = { 0, };
+void DO_playCA(void) {
+	int i, j, k, t;
 	int dx[8] = { -1,0,1,-1,1,-1,0,1 }, dy[8] = { 1,1,1,0,0,-1,-1,-1 };
 
 	for (t = 0; t < PLAYTIME; t++)
@@ -150,18 +166,17 @@ int main()
 
 		memset(matrix2, 0, sizeof(matrix2));
 
-//		printf(":%d ", t); PrintMatrix("PLAYING");
+		//		printf(":%d ", t); PrintMatrix("PLAYING");
 	}
+}
 
-	//=================세포 자동자 실행======================
-	//======================================================
-	//====================문자로 변환========================
-
+void DO_transform_to_string(void) {
+	int i = 0, j = 0, t, k, num = 0;
+	
 	PrintMatrix(":END_PLAY");
 	printf("Hash Code:\n");
-	
-	i = j = 0;
-	while (!(i == HEIGHT-1 && j+5 >= WIDTH))
+
+	while (!(i == HEIGHT - 1 && j + 5 >= WIDTH))
 	{
 		num = 0;
 		for (t = 0; t < 6; t++, j++) {
@@ -183,11 +198,5 @@ int main()
 	}
 	num /= 2;
 	printf("%c\n", hashtable[num]);
-
-	//====================문자로 변환========================
-	//======================================================
-
-	system("pause");
-
-	return 0;
 }
+
