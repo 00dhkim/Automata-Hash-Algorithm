62d0d1273a9b67118645eb1f1207868ab4feb6a3
kimdo331 <kimdo331@naver.com>
2019-09-21 17:09:22 +0900

    HEIGHT, WIDTH 가변화, input+salt 및 배열 일관화
    매크로 상수로 폭과 높이 변경 가능
    기존의 input따로, salt따로 배치하던 구조 -> input+salt 합쳐서 책읽듯이 배치 (줄바꿈 구현 완료)

--- AutomataHashAlgorithm.cpp
+++ AutomataHashAlgorithm.cpp
@@ -1,17 +1,26 @@
-﻿#include <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 #include <Windows.h>
 #include <string.h>
 
 #define PLAYTIME 100
+#define HEIGHT 32
+#define WIDTH 23
 
 char hashtable[64] = {
-						'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '/' 
+						'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 
+						'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 
+						'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 
+						'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 
+						'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 
+						'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 
+						'W', 'X', 'Y', 'Z', '0', '1', '2', '3', 
+						'4', '5', '6', '7', '8', '9', '+', '-' 
 };
 //BASE64 table
-char input[96], salt[32], output[128];
-int matrix[32][24] = { 0, };
+char input[96+32], salt[32], output[128];
+int matrix[HEIGHT][WIDTH] = { 0, };
 
 void swap(int &a, int &b)
 {
@@ -33,25 +42,27 @@ int CharToDec(char c)
 	return i;
 }
 
-void PrintMatrix()
+void PrintMatrix(const char str[])
 {
-	system("cls");
-	for (int i = 0; i < 32; i++)
+	printf("%s\n", str);
+	for (int i = 0; i < HEIGHT; i++)
 	{
-		for (int j = 0; j < 24; j++)
+		for (int j = 0; j < WIDTH; j++)
 		{
 			if (matrix[i][j]) printf("■");
 			else printf("□");
-			if (!((j+1) % 4)) printf(" ");
+//			if (!((j+1) % 6)) printf(" ");
 		}
 		printf("\n");
 	}
 	system("pause");
+	system("cls");
 }
 
 int main()
 {
-	int i, j, k, num;
+	int i, j, k, num, t;
+	char binary[7];
 
 	srand((int)time(NULL));
 
@@ -59,6 +70,9 @@ int main()
 	scanf("%s", input);
 	printf("salt: ");
 	scanf("%s", salt);
+	strcat(input, salt);
+
+	system("pause"); system("cls");
 
 /*	//random input & salt
 	for (i = 0; i < 96; i++)
@@ -68,63 +82,63 @@ int main()
 */	
 
 	//======================================================
-	//====================격자로 변환=======================
-
-	int t = 0;	//t는 매트릭스에 값 넣을때 첫째 라인에서 둘째 라인으로 넘어가는 과정에서 문자가 부드럽게 지속적으로 들어가도록 하기 위한 변수임. 이 값은 계속 1씩 더해지고 t를 inputzise로 나눈 나머지 값에 해당하는 input값을 2진수로 변환하면 됨. 따라서 t는 종료시 96임. (j는 0~32를 3번 반복하는 반면에)
-	for (i = 0; i < 3; i++)
+	//====================격자로 변환========================
+	
+	t = 0;
+	i = j = 0;
+	while (!(i == HEIGHT-1 && j+6 > WIDTH-1))	//matrix다 채우면 탈출하기
 	{
-		for (j = 0; j < 32; j++)
+		num = CharToDec(input[t++]);
+		strcpy(binary, "000000");
+		for (k = 5; k >= 0; k--)	// 문자를 2진수로 만들기
 		{
-			num = CharToDec(input[t%strlen(input)]);
+			if (num % 2 == 1) binary[k] = '1';
+			num /= 2;
+		}
+		if (t == strlen(input)) t = 0;
 
-			for (k = 5; k >= 0; k--)
-			{
-				matrix[j][8 * i + k] = num % 2;
-				num /= 2;
-//				PrintMatrix();
+		// 2진수를 matrix[][]안에 담기
+		if (j + 5 >= WIDTH) {	// 내림 해야하면
+			for (k = 0; k < WIDTH - j; k++) {
+				matrix[i][j + k] = (binary[k] == '1' ? 1 : 0);
 			}
-			t++;
+			i++; j = 0;
+			for (; k < 6; k++, j++) {
+				matrix[i][j] = (binary[k] == '1' ? 1 : 0);
+			}
+		}
+		else {	// 바로 넣을 수 있으면
+			for (k = 0; k < 6; k++) {
+				matrix[i][j+k] = (binary[k] == '1' ? 1 : 0);
+			}
+			j += 6;
 		}
 	}
-
-	//salt 첨가
-	for (i = 0; i < 32; i++)
-	{
-		int num = CharToDec(salt[i%strlen(salt)]);
-		matrix[i][23] = num % 2;
-		num /= 2;
-		matrix[i][22] = num % 2;
-		num /= 2;
-		matrix[i][15] = num % 2;
-		num /= 2;
-		matrix[i][14] = num % 2;
-		num /= 2;
-		matrix[i][7] = num % 2;
-		num /= 2;
-		matrix[i][6] = num % 2;
-//		PrintMatrix();
+	
+	for (k = 0; k < WIDTH - j; k++) { //남은 0~5칸 채우기
+		matrix[i][j + k] = (binary[k] == '1' ? 1 : 0);
 	}
-//	PrintMatrix();
-	//====================격자로 변환=======================
-	//======================================================
-	//=================세포 자동자 실행=====================
+	PrintMatrix(":INIT_MATRIX");
 
-	int matrix2[32][24] = { 0, };
+	//====================격자로 변환========================
+	//======================================================
+	//=================세포 자동자 실행======================
+
+	int matrix2[HEIGHT][WIDTH] = { 0, };
 	int dx[8] = { -1,0,1,-1,1,-1,0,1 }, dy[8] = { 1,1,1,0,0,-1,-1,-1 };
 
-//	PrintMatrix();
-	for (int q = 0; q < PLAYTIME; q++)
+	for (t = 0; t < PLAYTIME; t++)
 	{
 		//주위 개수 계산
-		for (i = 0; i < 32; i++)
-			for (j = 0; j < 24; j++)
+		for (i = 0; i < HEIGHT; i++)
+			for (j = 0; j < WIDTH; j++)
 				for (k = 0; k < 8; k++)
-					if (matrix[(i + dx[k] + 32) % 32][(j + dy[k] + 24) % 24]) matrix2[i][j]++;	//토러스형 격자
+					if (matrix[(i + dx[k] + HEIGHT) % HEIGHT][(j + dy[k] + WIDTH) % WIDTH]) matrix2[i][j]++;	//토러스형 격자
 
 		//규칙 판단
-		for (i = 0; i < 32; i++)
+		for (i = 0; i < HEIGHT; i++)
 		{
-			for (j = 0; j < 24; j++)
+			for (j = 0; j < WIDTH; j++)
 			{
 				if ((matrix2[i][j] == 1 || matrix2[i][j] == 2 || matrix2[i][j] == 3 || matrix2[i][j] == 7) && !matrix[i][j]) matrix[i][j] = 1;
 				else if ((matrix2[i][j] == 1 || matrix2[i][j] == 5 || matrix2[i][j] == 7) && matrix[i][j]) matrix[i][j] = 1;
@@ -134,31 +148,40 @@ int main()
 
 		memset(matrix2, 0, sizeof(matrix2));
 
-//		PrintMatrix();
+		printf(":%d ", t); PrintMatrix("PLAYING");
 	}
-//	PrintMatrix();
-	//=================세포 자동자 실행=====================
+	PrintMatrix(":END_PLAY"); 
+	//=================세포 자동자 실행======================
 	//======================================================
-	//====================문자로 변환=======================
+	//====================문자로 변환========================
 	
 	printf("Hash Code:\n");
-	for (i = 0; i < 32; i++)
+	
+	i = j = 0;
+	while (!(i == HEIGHT-1 && j+5 >= WIDTH))
 	{
-		for (j = 0; j < 4; j++)
-		{
-			num = 0;
-
-			for (k = 0; k < 6; k++)
-			{
-				num += matrix[i][6 * j + k];
-				num *= 2;
+		num = 0;
+		for (t = 0; t < 6; t++, j++) {
+			if (j == WIDTH) {
+				i++; j = 0;
 			}
-			num /= 2;
-			printf("%c", hashtable[num]);
+			num += matrix[i][j];
+			num *= 2;
 		}
+		num /= 2;
+		printf("%c", hashtable[num]);
 	}
-	printf("\n");
-	//====================문자로 변환=======================
+
+	// 0~5(WIDTH-j)개의 값을 matrix에서 가져오기
+	num = 0;
+	for (k = 0; k < WIDTH - j; k++) {
+		num += matrix[i][j + k];
+		num *= 2;
+	}
+	num /= 2;
+	printf("%c\n", hashtable[num]);
+
+	//====================문자로 변환========================
 	//======================================================
 
 	system("pause");
