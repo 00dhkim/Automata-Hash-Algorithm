c78a176452d4bed811925092dfc77dd1f2618d6f
kimdo331 <kimdo331@naver.com>
2019-09-23 14:16:40 +0900

    실행시 rt_pair.txt 차곡차곡 쌓기 완료
    처음에 pair 하나는 있어야 함. 실행하면 앞의 pair에서 input bits의 bit하나만 변형하여 새로운 pair 생성

--- rule-test/AHA_source_for_rule_test.cpp
+++ rule-test/AHA_source_for_rule_test.cpp
@@ -7,21 +7,87 @@
 #define PLAYTIME 100
 #define HEIGHT 32
 #define WIDTH 23
-#define IS_SALT 0
+#define IS_SALT 1
 
 char hashtable[64] = {
-						'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 
-						'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 
-						'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 
-						'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 
-						'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 
-						'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 
-						'W', 'X', 'Y', 'Z', '0', '1', '2', '3', 
-						'4', '5', '6', '7', '8', '9', '+', '-' 
+						'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+						'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+						'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
+						'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F',
+						'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
+						'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
+						'W', 'X', 'Y', 'Z', '0', '1', '2', '3',
+						'4', '5', '6', '7', '8', '9', '+', '-'
 };
 //BASE64 table
-char input[96+32], salt[32], output[128];
-int matrix[HEIGHT][WIDTH] = { 0, };
+char input[96 + 32], salt[32], binary[7];
+int matrix[HEIGHT][WIDTH] = { 0, }, matrix2[HEIGHT][WIDTH] = { 0, };
+
+void swap(int &a, int &b);
+int random(int a, int b);
+int CharToDec(char c);
+void PrintMatrix_rt();
+void fPrintMatrix_rt();
+void random_input_salt(int input_size, int salt_size);
+
+void DO_transform_to_matrix(void);
+void DO_playCA(void);
+void DO_transform_to_string(void);
+
+FILE *fp = fopen("rt_pair.txt", "a+");
+
+// 이전의 input bits를 읽은 뒤, 비트 하나만 바꾼 후 파일 맨 뒤에다 붙이기
+int main()
+{
+	srand((int)time(NULL));
+	char buf, buffer[20] = { 0, };
+	int cnt = 0, num = 0, i = 0;
+
+	fseek(fp, -((WIDTH + 2)*HEIGHT + 8), SEEK_END);
+	fgets(buffer, 7, fp);
+	
+	puts(buffer);
+	int rand_i = random(0, 5);
+	buffer[rand_i] = (buffer[rand_i] == '1' ? '0' : '1');
+	puts(buffer);
+	fseek(fp, 0, SEEK_END);
+	fprintf(fp, "%s\n", buffer);
+
+	for (i = 0; i < 6; i++) {
+		num += (buffer[i] == '1' ? 1 : 0);
+		num *= 2;
+	}
+	num /= 2;
+	input[0] = hashtable[num];
+
+/*	buf = fgetc(stdin);
+	while (buf != 10 && buf != EOF) {
+		if (buf == ' ') {
+			buf = fgetc(stdin);
+			continue;
+		}
+		num += (buf == '1' ? 1 : 0);
+		num *= 2;
+		if (++cnt == 6) {
+			cnt = 0;
+			input[i++] = hashtable[num / 2];
+			num = 0;
+		}
+		buf = fgetc(stdin);
+	}
+//	puts(input);
+*/
+	
+
+	DO_transform_to_matrix();	// 격자로 변환
+
+	DO_playCA();	// 세포 자동자 실행
+
+	fPrintMatrix_rt();
+
+	fclose(fp);
+	return 0;
+}
 
 void swap(int &a, int &b)
 {
@@ -43,52 +109,37 @@ int CharToDec(char c)
 	return i;
 }
 
-void PrintMatrix(const char str[])
+void PrintMatrix_rt()
 {
-//	printf("%s\n", str);
-	for (int i = 0; i < HEIGHT; i++)
-	{
-		for (int j = 0; j < WIDTH; j++)
-		{
-			if (matrix[i][j]) printf("■");
-			else printf("□");
-//			if (!((j+1) % 6)) printf(" ");
+	for (int i = 0; i < HEIGHT; i++) {
+		for (int j = 0; j < WIDTH; j++) {
+			printf("%d", matrix[i][j]);
 		}
 		printf("\n");
 	}
-//	system("pause");
-//	system("cls");
 }
 
-int main()
+void fPrintMatrix_rt()
 {
-	int i, j, k, num, t;
-	char binary[7];
-
-	srand((int)time(NULL));
-
-//	printf("input: ");
-	scanf("%s", input);
-	if (IS_SALT) {
-		printf("salt: ");
-		scanf("%s", salt);
-		strcat(input, salt);
+	for (int i = 0; i < HEIGHT; i++) {
+		for (int j = 0; j < WIDTH; j++) {
+			fprintf(fp, "%d", matrix[i][j]);
+		}
+		fprintf(fp, "\n");
 	}
-//	system("pause"); system("cls");
+}
 
-/*	//random input & salt
-	for (i = 0; i < 96; i++)
+void random_input_salt(int input_size, int salt_size) {
+	for (int i = 0; i < input_size; i++)
 		input[i] = hashtable[random(0, 63)];
-	for (i = 0; i < 32; i++)
+	for (int i = 0; i < salt_size; i++)
 		salt[i] = hashtable[random(0, 63)];
-*/	
+}
 
-	//======================================================
-	//====================격자로 변환========================
-	
-	t = 0;
-	i = j = 0;
-	while (!(i == HEIGHT-1 && j+6 > WIDTH-1))	//matrix다 채우면 탈출하기
+void DO_transform_to_matrix(void) {	//격자로 변환
+	int t = 0, i = 0, j = 0, k, num;
+
+	while (!(i == HEIGHT - 1 && j + 6 > WIDTH - 1))	//matrix다 채우면 탈출하기
 	{
 		num = CharToDec(input[t++]);
 		strcpy(binary, "000000");
@@ -111,22 +162,20 @@ int main()
 		}
 		else {	// 바로 넣을 수 있으면
 			for (k = 0; k < 6; k++) {
-				matrix[i][j+k] = (binary[k] == '1' ? 1 : 0);
+				matrix[i][j + k] = (binary[k] == '1' ? 1 : 0);
 			}
 			j += 6;
 		}
 	}
-	
+
 	for (k = 0; k < WIDTH - j; k++) { //남은 0~5칸 채우기
 		matrix[i][j + k] = (binary[k] == '1' ? 1 : 0);
 	}
-//	PrintMatrix(":INIT_MATRIX");
+	//	PrintMatrix(":INIT_MATRIX");
+}
 
-	//====================격자로 변환========================
-	//======================================================
-	//=================세포 자동자 실행======================
-
-	int matrix2[HEIGHT][WIDTH] = { 0, };
+void DO_playCA(void) {
+	int i, j, k, t;
 	int dx[8] = { -1,0,1,-1,1,-1,0,1 }, dy[8] = { 1,1,1,0,0,-1,-1,-1 };
 
 	for (t = 0; t < PLAYTIME; t++)
@@ -152,16 +201,15 @@ int main()
 
 //		printf(":%d ", t); PrintMatrix("PLAYING");
 	}
+}
 
-	//=================세포 자동자 실행======================
-	//======================================================
-	//====================문자로 변환========================
+void DO_transform_to_string(void) {
+	int i = 0, j = 0, t, k, num = 0;
 
-	PrintMatrix(":END_PLAY");
-/*	printf("Hash Code:\n");
-	
-	i = j = 0;
-	while (!(i == HEIGHT-1 && j+5 >= WIDTH))
+//	PrintMatrix(":END_PLAY");
+	printf("Hash Code:\n");
+
+	while (!(i == HEIGHT - 1 && j + 5 >= WIDTH))
 	{
 		num = 0;
 		for (t = 0; t < 6; t++, j++) {
@@ -183,11 +231,5 @@ int main()
 	}
 	num /= 2;
 	printf("%c\n", hashtable[num]);
-
-	//====================문자로 변환========================
-	//======================================================
-
-	system("pause");
-*/
-	return 0;
 }
+
