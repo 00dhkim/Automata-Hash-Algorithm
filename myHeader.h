9127350575ccaf04be7fbdeb64020c762c26de87
kimdo331 <kimdo331@naver.com>
2019-09-25 15:05:47 +0900

    Header 파일 추가

---
+++ myHeader.h
@@ -0,0 +1,32 @@
+#ifndef MYHEADER_H
+#define MYHEADER_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
+
+void swap(int &a, int &b);
+int random(int a, int b);
+int CharToDec(char c);
+void PrintMatrix(const char str[]);
+void random_input_salt(int input_size, int salt_size);
+
+void DO_transform_to_matrix(void);
+void DO_playCA(void);
+void DO_transform_to_string(void);
+
+void myPrint(const char str[], ...);	// Made by DongHwan
+
+constexpr auto PLAYTIME = 100;
+constexpr auto HEIGHT = 32;
+constexpr auto WIDTH = 23;
+constexpr auto IS_SALT = 0;
+constexpr auto IS_FILE_PRINT = 0;
+
+extern FILE *logfp;
+extern char hashtable[64];
+extern char input[96 + 32], salt[32], binary[7];
+extern int matrix[HEIGHT][WIDTH], matrix2[HEIGHT][WIDTH];
+
+#endif // MYHEADER_H
