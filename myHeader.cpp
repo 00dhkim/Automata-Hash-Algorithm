ace5df2cfdf9f3230f9f332a47d032a8ca3d9cce
kimdo331 <kimdo331@naver.com>
2019-09-26 14:35:25 +0900

    logPrint 으로 이름변경, 코딩스타일 변경

--- myHeader.cpp
+++ myHeader.cpp
@@ -16,38 +16,32 @@ char hashtable[64] = {
 char input[96 + 32], salt[32], binary[7];
 int matrix[HEIGHT][WIDTH] = { 0, }, matrix2[HEIGHT][WIDTH] = { 0, };
 
-void swap(int &a, int &b)
-{
+void swap(int &a, int &b) {
 	int t = a;
 	a = b;
 	b = t;
 }
 
-int random(int a, int b)
-{
+int random(int a, int b) {
 	return a + rand() % (b - a + 1);
 }
 
-int CharToDec(char c)
-{
+int CharToDec(char c) {
 	int i = 0;
 	while (hashtable[i] != c)
 		i++;
 	return i;
 }
 
-void PrintMatrix(const char str[])
-{
-	myPrint("%s\n", str);
-	for (int i = 0; i < HEIGHT; i++)
-	{
-		for (int j = 0; j < WIDTH; j++)
-		{
-			if (matrix[i][j]) myPrint("■");
-			else myPrint("□");
-			// if (!((j+1) % 6)) myPrint(" ");
+void PrintMatrix(const char str[]) {
+	logPrint("%s\n", str);
+	for (int i = 0; i < HEIGHT; i++) {
+		for (int j = 0; j < WIDTH; j++) {
+			if (matrix[i][j]) logPrint("■");
+			else logPrint("□");
+			// if (!((j+1) % 6)) logPrint(" ");
 		}
-		myPrint("\n");
+		logPrint("\n");
 	}
 	if (PRINT_MODE == 1) {
 		system("pause");
@@ -62,15 +56,13 @@ void random_input_salt(int input_size, int salt_size) {
 		salt[i] = hashtable[random(0, 63)];
 }
 
-void DO_transform_to_matrix(void) {	//격자로 변환
+void DO_transform_to_matrix(void) {	// 격자로 변환
 	int t = 0, i = 0, j = 0, k, num;
 
-	while (!(i == HEIGHT - 1 && j + 6 > WIDTH - 1))	//matrix다 채우면 탈출하기
-	{
+	while (!(i == HEIGHT - 1 && j + 6 > WIDTH - 1)) {	// matrix다 채우면 탈출하기
 		num = CharToDec(input[t++]);
 		strcpy(binary, "000000");
-		for (k = 5; k >= 0; k--)	// 문자를 2진수로 만들기
-		{
+		for (k = 5; k >= 0; k--) {	// 문자를 2진수로 만들기
 			if (num % 2 == 1) binary[k] = '1';
 			num /= 2;
 		}
@@ -104,8 +96,7 @@ void DO_playCA(void) {
 	int i, j, k, t;
 	int dx[8] = { -1,0,1,-1,1,-1,0,1 }, dy[8] = { 1,1,1,0,0,-1,-1,-1 };
 
-	for (t = 0; t < PLAYTIME; t++)
-	{
+	for (t = 0; t < PLAYTIME; t++) {
 		//주위 개수 계산
 		for (i = 0; i < HEIGHT; i++)
 			for (j = 0; j < WIDTH; j++)
@@ -113,10 +104,8 @@ void DO_playCA(void) {
 					if (matrix[(i + dx[k] + HEIGHT) % HEIGHT][(j + dy[k] + WIDTH) % WIDTH]) matrix2[i][j]++;	//토러스형 격자
 
 		//규칙 판단
-		for (i = 0; i < HEIGHT; i++)
-		{
-			for (j = 0; j < WIDTH; j++)
-			{
+		for (i = 0; i < HEIGHT; i++) {
+			for (j = 0; j < WIDTH; j++) {
 				if ((matrix2[i][j] == 1 || matrix2[i][j] == 2 || matrix2[i][j] == 3 || matrix2[i][j] == 7) && !matrix[i][j]) matrix[i][j] = 1;
 				else if ((matrix2[i][j] == 1 || matrix2[i][j] == 5 || matrix2[i][j] == 7) && matrix[i][j]) matrix[i][j] = 1;
 				else matrix[i][j] = 0;
@@ -125,7 +114,7 @@ void DO_playCA(void) {
 
 		memset(matrix2, 0, sizeof(matrix2));
 
-		myPrint(":%d ", t); PrintMatrix("PLAYING");
+		logPrint(":%d ", t); PrintMatrix("PLAYING");
 	}
 }
 
@@ -133,10 +122,9 @@ void DO_transform_to_string(void) {
 	int i = 0, j = 0, t, k, num = 0;
 
 	PrintMatrix(":END_PLAY");
-	myPrint("Hash Code:\n");
+	logPrint("Hash Code:\n");
 
-	while (!(i == HEIGHT - 1 && j + 5 >= WIDTH))
-	{
+	while (!(i == HEIGHT - 1 && j + 5 >= WIDTH)) {
 		num = 0;
 		for (t = 0; t < 6; t++, j++) {
 			if (j == WIDTH) {
@@ -146,7 +134,7 @@ void DO_transform_to_string(void) {
 			num *= 2;
 		}
 		num /= 2;
-		myPrint("%c", hashtable[num]);
+		logPrint("%c", hashtable[num]);
 	}
 
 	// 0~5(WIDTH-j)개의 값을 matrix에서 가져오기
@@ -156,10 +144,10 @@ void DO_transform_to_string(void) {
 		num *= 2;
 	}
 	num /= 2;
-	myPrint("%c\n", hashtable[num]);
+	logPrint("%c\n", hashtable[num]);
 }
 
-void myPrint(const char str[], ...) {
+void logPrint(const char str[], ...) {
 	va_list arg;
 
 	va_start(arg, str);
